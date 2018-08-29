024a8b8e04fa1126eb626a46b45dbef32004d869
David Guyo <David_Guyo@epam.com>
2018-08-29 12:25:57 +0300

    step 2

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -1,11 +1,20 @@
 package quoters;
 
+@Profiling
 public class TerminatorQuoter implements Quoter {
-
     @InjectRandomInt(min = 2, max = 7)
     private int repeat;
 
     private String message;
+
+    public void init(){
+        System.out.println("Phase 2");
+        System.out.println(repeat);
+    }
+
+    public TerminatorQuoter() {
+        System.out.println("Phase 1");
+    }
 
     @Override
     public void sayQuote() {
