23aa2a85987adf9f9189ab37334459cf0991e966
David Guyo <David_Guyo@epam.com>
2018-08-29 23:50:14 +0300

    step 5

---
+++ src/main/java/quoters/DeprecationHanglerBeanFactoryPostProcessor.java
@@ -0,0 +1,8 @@
+package quoters;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class DeprecationHanglerBeanFactoryPostProcessor implements BeanFactoryPostProcessor{
+}
--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -3,6 +3,7 @@ package quoters;
 import javax.annotation.PostConstruct;
 
 @Profiling
+@DeprecatedClass(newImpl = T1000.class)
 public class TerminatorQuoter implements Quoter {
     @InjectRandomInt(min = 2, max = 7)
     private int repeat;
@@ -19,7 +20,7 @@ public class TerminatorQuoter implements Quoter {
     }
 
     @Override
-    @PostProxe
+    @PostProxy
     public void sayQuote() {
         System.out.println("Phase 3");
         for (int i = 0; i < repeat; i++) {
